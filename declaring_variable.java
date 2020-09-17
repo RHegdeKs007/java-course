9c2aceaec721b422033c0c108ad6b34008b2eb0f
RHegdeKs007 <64642261+RHegdeKs007@users.noreply.github.com>
2020-09-17 18:12:06 +0530

    Add files via upload

---
+++ declaring_variable.java
@@ -0,0 +1,36 @@
+package cs046;
+
+public class declaring_variable {
+	public static void main(String[] args) {
+		 String name= "Rakshith";
+		 System.out.println(name);
+		
+		 int num=46;
+		  System.out.println(num);
+		//or
+		 int mynum;
+	    	mynum=07;
+	    	System.out.println(mynum);
+	     //or
+		int urnum=20;
+	      urnum=47;
+	       System.out.println(urnum);
+	    
+	    final int i=57;
+	    	System.out.println(i);
+	     
+	   float wnum=5.99f;
+	       System.out.println(wnum);
+	      
+	    byte ee=100;
+	       System.out.println(ee);
+	   
+	   short po=4000;
+	   System.out.println(po);
+	   
+	   long r=10000000L;
+	   System.out.println(r);
+	    
+	}
+
+}
