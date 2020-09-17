669087b32422deb87400727dbe98d983470f39af
RHegdeKs007 <64642261+RHegdeKs007@users.noreply.github.com>
2020-09-17 18:23:43 +0530

    Add files via upload

---
+++ mathclass.java
@@ -0,0 +1,24 @@
+package cs046;
+
+public class mathclass {
+        public static void main(String[] args) {
+        	//max of two numbers
+        	int x=10;
+        	int y=5;
+        	System.out.println(Math.max(x, y));
+        	//min of two numbers
+        	int a=50;
+        	int b=4;
+        	System.out.println(Math.min(a, b));
+        	//sqrt of a number
+        	int x1=144;
+        	System.out.println(Math.sqrt(x1));
+        	//absolute(positive) of a number
+        	double r=-4.98d;
+        	System.out.println(Math.abs(r));
+        	//to generate random number
+        	int x5=(int)(Math.random() *90);
+        	System.out.println(x5);
+        	
+        }
+}
