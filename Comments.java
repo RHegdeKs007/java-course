13af12a1c8596abbe7984b9a6b9f3b01c0b550f3
RHegdeKs007 <64642261+RHegdeKs007@users.noreply.github.com>
2020-09-17 18:13:51 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,13 @@
+package cs046;
+
+public class Comments {
+	public static void main(String[] args) {
+		 System.out.println("hi");
+		 //hi i am rk.
+		 /*
+		  i am studying in jit
+		  it is located in thathguni.
+		  */
+	}
+
+}
