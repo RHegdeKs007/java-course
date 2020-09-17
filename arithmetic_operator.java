6a1d6169edb42cb9a72548ef969e77cb62d372b4
RHegdeKs007 <64642261+RHegdeKs007@users.noreply.github.com>
2020-09-17 18:16:10 +0530

    Add files via upload

---
+++ arithmetic_operator.java
@@ -0,0 +1,47 @@
+package cs046;
+
+public class arithmetic_operator {
+	public static void main(String[] args ) {
+		//addition
+		int x=30+40;
+		System.out.println(x);
+		//or
+		int sum1=30+40;
+		int sum2=sum1+90;
+		int sum3=sum1+sum2;
+		System.out.println(sum1);
+		System.out.println(sum2);
+		System.out.println(sum3);
+		
+		//subtraction
+		int x1=30;
+		int x2=24;
+		System.out.println(x1-x2);
+		
+		//multiplication
+		int y1=46;
+		int y2=8;
+		System.out.println(y1*y2);
+		
+		//division
+		int z1=63;
+		int z2=7;
+		System.out.println(z1/z2);
+		
+		//modulus
+		int r1=47;
+		int r2=21;
+		System.out.println(r1%r2);
+		
+		//increment
+		int e=6;
+		e++;
+		System.out.println(e);
+		
+		//decrement
+		int w=8;
+		--w;
+		System.out.println(w);
+	}
+
+}
